cbf13dc4ec7f5af3eab5467633f0e27b0e53926a
Tan Le <tanlethanh8266@gmail.com>
2024-07-27 18:12:16 +0700

    chore: use configs param for chrome channel config

--- src/chrome/channel.ts
+++ src/chrome/channel.ts
@@ -1,24 +1,25 @@
 import { AsyncChannel } from '../core/channel';
 import type { RawRequest } from '../core/types';
 
+type ChromeChannelConfigs = {
+	/**
+	 * use autoReconnect will listen to disconnect event and pageshow from bfcache to reconnect to the kernel
+	 */
+	autoReconnect?: boolean;
+};
+
 export class ChromeChannel extends AsyncChannel {
 	connection: chrome.runtime.Port;
 
-	constructor(
-		channelId: string,
-		/**
-		 * use autoReconnect will listen to disconnect event and pageshow from bfcache to reconnect to the kernel
-		 */
-		autoReconnect?: boolean,
-	) {
+	constructor(channelId: string, configs?: ChromeChannelConfigs) {
 		super();
-		this.connection = this.connect(channelId, autoReconnect);
+		this.connection = this.connect(channelId, configs?.autoReconnect);
 
-		if (autoReconnect) {
+		if (configs?.autoReconnect) {
 			window?.addEventListener('pageshow', (event) => {
 				if (event.persisted) {
 					console.warn('Page restored from bfcache, reconnect');
-					this.connection = this.connect(channelId, autoReconnect);
+					this.connection = this.connect(channelId, configs.autoReconnect);
 				}
 			});
 		}
