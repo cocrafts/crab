bad8c4ed2b57aaa80ab9203735978da2f1485e93
Tan Le <tanlethanh8266@gmail.com>
2024-07-27 18:08:19 +0700

    feat: add autoReconnect options for channel to reconnect when port disconnected or pageshow from bfcache

--- src/chrome/channel.ts
+++ src/chrome/channel.ts
@@ -4,12 +4,43 @@ import type { RawRequest } from '../core/types';
 export class ChromeChannel extends AsyncChannel {
 	connection: chrome.runtime.Port;
 
-	constructor(channelId: string) {
+	constructor(
+		channelId: string,
+		/**
+		 * use autoReconnect will listen to disconnect event and pageshow from bfcache to reconnect to the kernel
+		 */
+		autoReconnect?: boolean,
+	) {
 		super();
-		this.connection = chrome.runtime.connect({ name: channelId });
-		this.connection.onMessage.addListener((message) => {
+		this.connection = this.connect(channelId, autoReconnect);
+
+		if (autoReconnect) {
+			window?.addEventListener('pageshow', (event) => {
+				if (event.persisted) {
+					console.warn('Page restored from bfcache, reconnect');
+					this.connection = this.connect(channelId, autoReconnect);
+				}
+			});
+		}
+	}
+
+	private connect(
+		channelId: string,
+		autoReconnect?: boolean,
+	): chrome.runtime.Port {
+		const connection = chrome.runtime.connect({ name: channelId });
+		connection.onMessage.addListener((message) => {
 			this.handleIncoming(message);
 		});
+
+		if (autoReconnect) {
+			connection.onDisconnect.addListener(() => {
+				console.warn('Port disconnected, attempting to reconnect...');
+				this.connection = this.connect(channelId, autoReconnect);
+			});
+		}
+
+		return connection;
 	}
 
 	push(payload: RawRequest): void {
